0fe486af19704d8b58c0f67f338555fb7ff25480
Sra Suzuki <alesuzuki@gmail.com>
2024-05-07 12:30:32 -0300

    testando outras formas de namespace

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 ﻿//Console.WriteLine("Hello, World!");
 
+
+// Posso fazer desta forma
+/*
 using estrutura.Models;
 
 Pessoa p = new Pessoa();
+*/
+
+
+
+//Ou posso fazer desta outra forma para referenciar o namespace
+//negativas de usar desta forma: O código fica extenso
+estrutura.Models.Pessoa p = new estrutura.Models.Pessoa();
+
 
