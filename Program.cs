e3907fa3c670280304751fb55d765d8f3d17166c
Sra Suzuki <alesuzuki@gmail.com>
2024-05-07 12:45:01 -0300

    atribuindo nome e idade e chamando a funcao de apresentar

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ using estrutura.Models;
 
 Pessoa p = new Pessoa();
 
+p.Nome = "Alessandra Pasquale";
+p.Idade = 35;
+p.Apresentar();
+
 
 //Ou posso fazer desta outra forma para referenciar o namespace
 //negativas de usar desta forma: O código fica extenso
