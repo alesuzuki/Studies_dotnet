4fadd6a8ad5ce2068a89a56e5a61f7a7d98d26a6
Sra Suzuki <alesuzuki@gmail.com>
2024-05-07 12:48:01 -0300

    adicionado comentários dos estudos para ter a explicação linha a linha do que foi feito

--- Program.cs
+++ Program.cs
@@ -3,10 +3,16 @@
 // Posso fazer desta forma
 using estrutura.Models;
 
+///instanciamos uma pessoa na variavel p
+//passa a existir e eu posso manipular essa instancia na variavel p
 Pessoa p = new Pessoa();
 
+//Aqui eu passo dados para pessoa e atribuo valores aos atributos Nome e idade
 p.Nome = "Alessandra Pasquale";
 p.Idade = 35;
+
+//Aqui chamo o método de apresentar e executa a apresentação 
+//quando dou o comando "dotnet run" no terminal
 p.Apresentar();
 
 
