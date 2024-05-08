2ec746b0cbe5bf881cfe1ca47975f5ac4e38ef7e
Sra Suzuki <alesuzuki@gmail.com>
2024-05-08 12:23:22 -0300

    Combinando operadores

---
+++ 03_estudos_de_operadores/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// Estudando os tipos de operadores
+// Operador de Atribuição
+// Operador de Condicionais
+// Operador de Lógicos
+
+// Combinando operadores
+
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+//Ao invés de fazer desta forma, 
+//podemos acrescer e atribuir ao mesmo tempo
+//utilizando os sinais +=
+c = c + 5;
+
+//fazendo desta maneira
+//uma maneira mais concisa
+//escrevo menos codigo e represneto a mesma coisa
+//da para usar subtracao -, soma +, divisao /, multiplicacao *
+c += 5;
+
+Console.WriteLine(c);
+
+
+
+
+
+/*
+// Estudando os tipos de operadores
+// Operador de Atribuição
+
+// sinal de = 
+// Para modificar o valor da variavel
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+
+Console.WriteLine(c);
+
+*/
