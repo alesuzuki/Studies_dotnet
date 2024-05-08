bd70f04d8fdbf08298b1b7a94d62c8f7f8cdee40
Sra Suzuki <alesuzuki@gmail.com>
2024-05-08 10:57:03 -0300

    testando tipos de variaveis

---
+++ 02_tipos_de_dados/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+string apresentacao = "Olá, seja bem vindo";
+
+int     quantidade = 1;
+double  altura = 1.68;
+decimal preco = 1.80M;
+bool    condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
