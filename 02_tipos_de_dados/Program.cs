dd733fd2db570ae44ba99999dec1e9f7f089a828
Sra Suzuki <alesuzuki@gmail.com>
2024-05-08 11:04:46 -0300

    formatacao de numero do tipo double com mais casas decimais

--- 02_tipos_de_dados/Program.cs
+++ 02_tipos_de_dados/Program.cs
@@ -4,12 +4,13 @@
 string apresentacao = "Olá, seja bem vindo";
 
 int     quantidade = 1;
-double  altura = 1.68;
+double  altura = 1.70;
 decimal preco = 1.80M;
 bool    condicao = true;
 
 Console.WriteLine(apresentacao);
 Console.WriteLine("Valor da variável quantidade: " + quantidade);
 Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável altura com mais casas decimais: " + altura.ToString("0.00"));
 Console.WriteLine("Valor da variável preco: " + preco);
 Console.WriteLine("Valor da variável condicao: " + condicao);
--- 02_tipos_de_dados/obj/Debug/net8.0/02_tipos_de_dados.AssemblyInfo.cs
+++ 02_tipos_de_dados/obj/Debug/net8.0/02_tipos_de_dados.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02_tipos_de_dados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9929e0cc7fb6f38ec31e55e97ac4d23f20d8a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd70f04d8fdbf08298b1b7a94d62c8f7f8cdee40")]
 [assembly: System.Reflection.AssemblyProductAttribute("02_tipos_de_dados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02_tipos_de_dados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
