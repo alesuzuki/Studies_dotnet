9c0b8da5335ac3fa742a310d1dc07ca3ae0ad98b
Sra Suzuki <alesuzuki@gmail.com>
2024-05-22 12:11:41 -0300

    serializacao e utilização do json e escrita pro arquivo

--- 10_propriedadesMetodosConstrutores/Program.cs
+++ 10_propriedadesMetodosConstrutores/Program.cs
@@ -6,6 +6,8 @@ Venda v1 = new Venda (1, "Material de escritório", 25.00M);
 
 string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
 
+File.WriteAllText ("Arquivos/venda.json", serializado);
+
 Console.WriteLine (serializado);
 
 
